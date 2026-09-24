0876e927b4cdb091e4d7e81042181d11ea75fd08
gabrielnagatanicampos <gabriel.nagatani14@gmail.com>
2026-09-24 10:58:59 -0300

    Feat: add persistência de dados, inserindo ticker e quantidade no BD

--- database.py
+++ database.py
@@ -10,8 +10,24 @@ cursor.execute("""
                 username TEXT NOT NULL UNIQUE,
                 password_hash TEXT NOT NULL,
                 salt TEXT NOT NULL,
-                created_on DATETIME DEFAULT CURRENT_TIMESTAMP
+                created_on DATETIME DEFAULT CURRENT_TIMESTAMP      
+            
                )""")
+
+conexao.commit()
+
+
+cursor.execute("""
+            CREATE TABLE IF NOT EXISTS carteira(
+            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            usuario_id INTEGER NOT NULL,
+            ticker TEXT NOT NULL,
+            quantidade INTEGER NOT NULL,
+            criado_em DATETIME DEFAULT CURRENT_TIMESTAMP,
+            FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
+            UNIQUE(usuario_id, ticker)          
+            )  """)
+
 
 conexao.commit()
 
@@ -50,8 +66,57 @@ def search_user(username):
         conexao.close()
     
 
-def insert_transaction():
-    pass
+def insert_ticker(usuario_id, ticker, qnt):
+    conexao = sqlite3.connect('banco.db')
+    conexao.row_factory = sqlite3.Row
+    
+    
+    
+    try:
+        cursor = conexao.cursor()
+    
+        sql = """
+           INSERT INTO carteira(usuario_id, ticker, quantidade)
+           VALUES (?, ?, ?)
+           ON CONFLICT(usuario_id, ticker) DO UPDATE SET 
+                quantidade = excluded.quantidade
+        """
+            
+        cursor.execute(sql, (usuario_id, ticker, qnt))
+        conexao.commit()
+    finally:
+        conexao.close()
+        
+      
+        
+def read_ticker(usuario_id):
+    conexao = sqlite3.connect('banco.db')
+    conexao.row_factory = sqlite3.Row
+    try:
+        cursor = conexao.cursor()
 
-def list_transaction():
+        sql = """
+            SELECT ticker, quantidade
+            FROM carteira
+            WHERE usuario_id = ?
+            """
+        
+        cursor.execute(sql, (usuario_id,))
+    
+        ticker = cursor.fetchall()
+    
+
+        return ticker
+    finally:
+        conexao.close()
+        
+    
+
+
+def delete_transaction():
     pass   
+
+
+insert_ticker(1,'PETR3', 30)
+
+print(read_ticker(1))
